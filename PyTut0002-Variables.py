e3b5aefb3f117c79b51b066cf4df267a56ee0c07
BOUAZIZI <bouazizi-atef@outlook.com>
2025-09-06 11:37:38 +0100

    Add files via upload

---
+++ PyTut0002-Variables.py
@@ -0,0 +1,61 @@
+print('\t\t *******************************')
+print('\t\t **    Python-Tutorial0001    **')
+print('\t\t **===========================**')
+print('\t\t ** Developer : BOUAZIZI ATEF **')
+print('\t\t ** BOUAZIZI-ATEF@OUTLOOK.COM **')
+print('\t\t *******************************\n')
+#------------------------------------------------
+input('Press enter to continue...')
+#------------------------------------------------
+print('1. int (entiers)')
+age = 25
+print(age)
+print(type(age))  # <class 'int'>
+print('---------------\n')
+
+print('2. float (nombres à virgule flottante)')
+prix = 19.99
+print(prix)
+print(type(prix))  # <class 'float'>
+print('---------------\n')
+
+print('3. str (chaînes de caractères)')
+nom = "Alice"
+print(nom)
+print(type(nom))  # <class 'str'>
+print('---------------\n')
+
+print('4. bool (booléens)')
+majeur = True
+print(majeur)
+print(type(majeur))  # <class 'bool'>
+print('---------------\n')
+
+print('5. list (listes)')
+fruits = ["pomme", "banane", "orange"]
+print(fruits)
+print(type(fruits))  # <class 'list'>
+print('---------------\n')
+
+print('6. tuple (tuples)')
+coordonnees = (10.0, 20.0)
+print(coordonnees)
+print(type(coordonnees))  # <class 'tuple'>
+print('---------------\n')
+
+print('7. dict (dictionnaires)')
+personne = {
+    "nom": "Bob",
+    "age": 30
+}
+print(personne)
+print(type(personne))  # <class 'dict'>
+print('---------------\n')
+
+print('8. set (ensembles)')
+couleurs = {"rouge", "vert", "bleu"}
+print(couleurs)
+print(type(couleurs))  # <class 'set'>
+#------------------------------------------------
+print('\n')
+input('Press enter to exit!..')
