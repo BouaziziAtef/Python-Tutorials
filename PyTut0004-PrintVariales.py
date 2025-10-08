f8003bc907f3d08e6adbe8f8852407843e0eb4ef
BOUAZIZI <bouazizi-atef@outlook.com>
2025-10-08 11:31:44 +0100

    Add files via upload

---
+++ PyTut0004-PrintVariales.py
@@ -0,0 +1,27 @@
+print('\t\t *******************************')
+print('\t\t **    Python-Tutorial0001    **')
+print('\t\t **===========================**')
+print('\t\t ** Developer : BOUAZIZI ATEF **')
+print('\t\t ** BOUAZIZI-ATEF@OUTLOOK.COM **')
+print('\t\t *******************************')
+#------------------------------------------------
+input('\nPress enter to continue...')
+#------------------------------------------------
+
+# Perform a basic calculation
+Num1 = 10
+Num2 = 5
+Result = Num1 + Num2
+
+print("Num1 = 10")
+print("Num2 = 5")
+print("Result = Num1 + Num2")
+print('\n')
+
+print("print('The sum of',Num1,'and',Num2,'is',Result,'.') ==> The sum of",Num1,"and",Num2,"is",Result,".")
+print("print(f'The sum of {Num1} and {Num2} is {Result}.') ==> "f"The sum of {Num1} and {Num2} is {Result}.")
+
+#------------------------------------------------
+print('\n')
+input('Press enter to exit!..')
+
